b9733df84cd92653d9a7b9811fed0d0467eafc40
Al Pagan <generamics@gmail.com>
2019-08-25 11:52:09 +0100

    Updates

---
+++ precache-manifest.4d5fe9e26259f7a4beb653b1b4fc58a6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da5c697bf78f33c864ac5d9c6c5354d8",
+    "url": "/prospero-landing-page/index.html"
+  },
+  {
+    "revision": "4cb32e31bf89568e6f83",
+    "url": "/prospero-landing-page/static/css/main.543fa7be.chunk.css"
+  },
+  {
+    "revision": "1e47ed1e5140b7ad3e8f",
+    "url": "/prospero-landing-page/static/js/2.5b00826a.chunk.js"
+  },
+  {
+    "revision": "4cb32e31bf89568e6f83",
+    "url": "/prospero-landing-page/static/js/main.7bec7539.chunk.js"
+  },
+  {
+    "revision": "1e891a63c67db851a013",
+    "url": "/prospero-landing-page/static/js/runtime~main.688c0c85.js"
+  }
+]);
